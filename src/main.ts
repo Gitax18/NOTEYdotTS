f3777be567004feaea5f3251b8e7b063927680b8
Gitax18 <lusifer0418@gmail.com>
2025-02-06 18:56:08 +0530

    add logic to handle archive notes

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,14 @@ const hideForm = document.getElementById("form-close-btn");
 // Note Element
 const notesContainer = document.getElementById("notes");
 const noteMenuBtns = document.getElementsByClassName("note-option");
-const noteArchiveBtn = document.getElementsByClassName("note-archive-btn");
+const noteArchiveBtns = document.getElementsByClassName("note-archive-btn");
 const noteDeleteBtns = document.getElementsByClassName("note-delete-btn");
+// Archive Element
+const archiveContainer = document.getElementById("archive-notes");
+const archiveDeleteBtns = document.getElementsByClassName("archive-delete-btn");
 // Managing state
 const store = JSON.parse(localStorage.getItem("notes") || "[]");
+const archive = JSON.parse(localStorage.getItem("archive") || "[]");
 
 // creating note element for existing notes
 if (store)
@@ -22,16 +26,39 @@ if (store)
     if (notesContainer) notesContainer.appendChild(newNote.noteElement());
   });
 
+// creating archive note elements for archived notes
+if (archive)
+  archive.forEach((note) => {
+    const newNote = new Note(note);
+    if (archiveContainer)
+      archiveContainer.appendChild(newNote.archiveNoteElement());
+  });
+
 // Handling note toggle menu
 for (const hamburger of noteMenuBtns) {
   hamburger.addEventListener("click", function () {
     // const menu = this.firstChild;
     const menu = this.firstElementChild;
     const style = window.getComputedStyle(menu);
-    console.log(style.visibility);
     if (style.visibility == "hidden") menu.style.visibility = "visible";
     else menu.style.visibility = "hidden";
   });
+}
+
+// handling archive addition
+let arh_count = 0;
+for (const btn of noteArchiveBtns) {
+  btn.addEventListener("click", function () {
+    store.pop(arh_count);
+    const note = this.closest(".note");
+    const title = note.querySelector(".note-title").innerText;
+    const description = note.querySelector(".note-description").innerText;
+    archive.push({ title, body: description });
+    localStorage.setItem("archive", JSON.stringify(archive));
+    localStorage.setItem("notes", JSON.stringify(store));
+    note.remove();
+  });
+  arh_count++;
 }
 
 // Handling note Deletion
@@ -46,6 +73,19 @@ for (const btn of noteDeleteBtns) {
   count++;
 }
 
+// Handling archive note Deletion
+let arh_count_del = 0;
+for (const btn of archiveDeleteBtns) {
+  btn.addEventListener("click", function () {
+    archive.pop(arh_count_del);
+    localStorage.setItem("archive", JSON.stringify(archive));
+
+    this.closest(".note").remove();
+  });
+  arh_count_del++;
+}
+
+// showing form
 showForm?.addEventListener("click", function (): void {
   if (formContainer) {
     formContainer.style.visibility = "visible";
@@ -53,6 +93,7 @@ showForm?.addEventListener("click", function (): void {
   }
 });
 
+// hiding form
 hideForm?.addEventListener("click", function (): void {
   if (formContainer) {
     formContainer.style.visibility = "hidden";
@@ -60,6 +101,7 @@ hideForm?.addEventListener("click", function (): void {
   }
 });
 
+// handling form
 form?.addEventListener("submit", function (e) {
   e.preventDefault();
   const data = {
